fb8d6d1f3ddc0086e5b1b61cf09b93438304ad71
carlosalfonso144 <carlosalfonso144@gmail.com>
2016-11-07 00:17:12 -0500

    mas cacharreos

--- js/home.js
+++ js/home.js
@@ -1,3 +1,5 @@
+
+
 //  Menu con cambio de color
 function menu(color){
     var a = document.getElementById("menu");
@@ -8,3 +10,22 @@ function menuc(){
     var a = document.getElementById("menu");
     a.className = ("invisible");
 }
+
+
+
+
+//  Menu con cambio de color
+function caja(color){
+    var a = document.getElementById("caja");
+    a.style.background = color;
+    a.className = ("");
+}
+function cajac(){
+    var a = document.getElementById("caja");
+    a.className = ("invisible2");
+}
+
+
+
+
+
