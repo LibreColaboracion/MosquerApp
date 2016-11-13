79a2444e03fad6be96e101c744cb3ea3fd403b54
MoiRouhs <carsrouhs@gmail.com>
2016-11-12 20:52:41 -0500

    Cambios para que trabje bien

--- js/contenidos.js
+++ js/contenidos.js
@@ -1,9 +1,12 @@
-var objetoJSON;
-    objetoJSON = {"titulo":"Iglesia Principal de Mosquera",
-                  "parrafo":"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."
+var objetoJSON = {
+		"titulo":"Iglesia Principal de Mosquera",
+		"parrafo":"Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."
                 };
-    
-    document.getElementById ("contenido").innerHTML = objetoJSON.titulo + " "+ objetoJSON.parrafo
+var d = document.getElementById ("contenido");
+console.log (objetoJSON.titulo);
+console.log (objetoJSON.parrafo);
+	d.innerHTML = "objetoJSON.titulo";
+    d.innerHTML =  objetoJSON.parrafo;
 
 
     
