3bbc7a9a7b8f34d78965f84ddd96e5b852d14ddd
carlosalfonso144 <carlosalfonso144@gmail.com>
2016-11-22 15:27:28 -0500

    peliando con css y javascrip

--- js/contenidos.js
+++ js/contenidos.js
@@ -1,7 +1,7 @@
 var iglesia = {
     'titulo':'Catedral de Mosquera',
     'Categorias':['cultura','turismo','religion'],
-    'historia':'Bla bla blalblalblablabla bla bla blasvla'
+    'historia':'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.'
     };
     
 var parque = {"titulo":'Parque principal',
@@ -17,22 +17,25 @@ var lugares = ['iglesia','piscina','parque','casa de la cultura','alcaldia','Per
 
 function vercontenido(argumento){ // argumentopodria ser cualquier palabra como lo hizo con iglesia
     console.log('hola')
-    var contenedor = document.getElementById('titulo'),
+    var contenedor = document.getElementById('titulo');
+        contenedor.className = ("");
+        contenedor.className = ("detallesvisto");
+        contenedor.innerHTML = argumento.titulo;
         parrafo = document.getElementById("parrafo");
-    contenedor.innerHTML = "<h1 id=titulo'>"+ argumento.titulo +"</h1>";
-    parrafo.innerHTML = argumento.historia
-    
+        parrafo.className = ("");
+        parrafo.className = ("detallesvisto");
+        parrafo.innerHTML = argumento.historia;
 };
 
 
                  
 
 window.addEventListener('load', function(){
-    verNuevaLista(lugares);
+    
     document.querySelector("#iglesia").addEventListener('click',function(){
                                             vercontenido(iglesia)}, false); 
     document.querySelector("#alcaldia").addEventListener('click',function(){
-                                            vercontenido(parque)}, false);
+                                            vercontenido(alcaldia)}, false);
     document.querySelector("#alcaldia").addEventListener('click',function(){
                                             vercontenido(piscina)}, false);
 
