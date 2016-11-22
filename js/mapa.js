816ffa8a3e0167dfa7913327542b68cfeeff0d00
carlosalfonso144 <carlosalfonso144@gmail.com>
2016-11-22 10:57:35 -0500

    nuevo oriente

---
+++ js/mapa.js
@@ -0,0 +1,4 @@
+function sizer(opt){
+var obj = document.currentImg;
+obj.width = eval(obj.width + opt + 1.5);
+}
---
+++ js/menu.js
@@ -0,0 +1,11 @@
+//  Menu con cambio de color
+function menu(color){
+    var a = document.getElementById("menu");
+    a.style.background = color;
+    a.className = ("");
+}
+function menuc(){
+    var a = document.getElementById("menu");
+    a.className = ("invisible");
+}
+
