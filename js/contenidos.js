f4890a6f4e49bfc10196393888910f5beab63f55
carlosalfonso144 <carlosalfonso144@gmail.com>
2016-11-22 14:39:08 -0500

    limpiando mugre

--- js/contenidos.js
+++ js/contenidos.js
@@ -1,44 +1,39 @@
-// esta seccion tendra los textos, considero parcialmente que
-//todo lo que sea Json este en un solo archivo.
 var iglesia = {
     'titulo':'Catedral de Mosquera',
     'Categorias':['cultura','turismo','religion'],
-    'historia':'<img class="galeria" src="lugares/lugar1/img/iglesia.png"> Bla bla blalblalblablabla bla bla blasvla'
-    
+    'historia':'Bla bla blalblalblablabla bla bla blasvla'
     };
     
 var parque = {"titulo":'Parque principal',
     'Categorias':['cultura','turismo'],
-    'historia': '<img class="galeria" src="lugares/lugar2/img/parque.png"> ble ble ble ble  djfhjksdhfjkdshfkjhsdkfjhsdjfhsjk'
+    'historia':'ble ble ble ble  djfhjksdhfjkdshfkjhsdkfjhsdjfhsjk'
 };
 var piscina = {"titulo":'Piscina Munisipal',
     'Categorias':['cultura','turismo','deporte'],
-    'historia':'<img class="galeria" src="lugares/lugar3/img/pisina.png">aguaagua agua agua agua agua agua agua'
+    'historia':'aguaagua agua agua agua agua agua agua'
 };
 
 var lugares = ['iglesia','piscina','parque','casa de la cultura','alcaldia','Personeria','casa de la juventud'];
 
-//esta funcion carga los textos en el html segun sea el click 
-function vercontenido(lugartexto){ 
+function vercontenido(argumento){ // argumentopodria ser cualquier palabra como lo hizo con iglesia
     console.log('hola')
-    var contenedor = document.getElementById("titulotex");
+    var contenedor = document.getElementById('titulo'),
         parrafo = document.getElementById("parrafo");
-        contenedor.innerHTML = "<h1 id='titulotex'>" + lugartexto.titulo +"</h1>";
-    	parrafo.innerHTML = lugartexto.historia;
-    	
+    contenedor.innerHTML = "<h1 id=titulo'>"+ argumento.titulo +"</h1>";
+    parrafo.innerHTML = argumento.historia
     
 };
 
 
-//esta funcion se usara para cargar automaticamente los nuevos lugares
-function verNuevaLista(){
-     for (i=0; i<= -1+ lugares.length; i++) {
-        console.log (lugares[i]);
-        var lista = document.getElementById("lista"); 
-        lista.innerHTML += lugares[i] + '<br>';
-     };
                  
-};
+
 window.addEventListener('load', function(){
-    verNuevaLista(lugares)
+    verNuevaLista(lugares);
+    document.querySelector("#iglesia").addEventListener('click',function(){
+                                            vercontenido(iglesia)}, false); 
+    document.querySelector("#alcaldia").addEventListener('click',function(){
+                                            vercontenido(parque)}, false);
+    document.querySelector("#alcaldia").addEventListener('click',function(){
+                                            vercontenido(piscina)}, false);
+
 });
--- js/mapa.js
+++
@@ -1,4 +0,0 @@
-function sizer(opt){
-var obj = document.currentImg;
-obj.width = eval(obj.width + opt + 1.5);
-}
