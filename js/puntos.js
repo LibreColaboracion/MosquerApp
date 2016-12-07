5b59156d3011ea469cfb1eb60a6c4409768d1980
carlosalfonso144 <carlosalfonso144@gmail.com>
2016-12-06 20:34:47 -0500

    pegando codifo de carlos rocha y ubucando puntos lugar real mas fondo blanco de titulo div

--- js/puntos.js
+++ js/puntos.js
@@ -1,10 +1,35 @@
+function hola(arg){
+    var f = document.getElementById('titulo');
+    f.innerHTML = "<h1>"+ arg +"</h1>" ;
+    f.style.borderRadius = "10px";
+    f.style.backgroundColor = "white";
+    console.log(arg);
+};
+var cordenadas = {
+    'Laguna la herrera':[58,142],
+    'Humedal Meandro del say':[391,193],
+    'Desierto De Sabrinsky':[84,199],
+    'Hacienda El Novillero':[170,122],
+    'Humedal guali':[285, 104],
+    'Piedras de Usca':[85, 173],
+    'Parque Principal Mosquera':[208, 110]
+    
+};
+
+function puntos(arg){
+    var corlugares = Object.keys(arg);
+    var a = document.getElementById('mapaex').getSVGDocument();
+    var c = a.getElementById('layer2');
+    // Me adelante un poco con esta sentencia que usted no sabe manejar pero es casi un for no por eso dice foreach = para cada uno
+        corlugares.forEach(function(e){
+            c.innerHTML += '<circle id="'+e+'" cx="'+arg[e][0]+'" cy="'+arg[e][1]+'" r="7" style="opacity:1;fill:#00ff00;fill-opacity:1;fill-rule:nonzero;stroke:#800000;stroke-width:1.37062943;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1"> </circle>';
+        });
+        corlugares.forEach(function(e){
+            a.getElementById(e).addEventListener('click',function(){hola(e)});
+        });
+};
 
 window.addEventListener('load', function(){
-    var a = document.getElementById('mapaex').getSVGDocument();// se selecciona la etiqueta objet
-    var punto = a.getElementById('punto'); // se seleciona el id dentro del codigo del svg
-    var c = a.getElementById('layer2');  // seleciono la etiqueta <g> mediante su id
-    c.innerHTML += '<circle id="iglesia" cx="80" cy="180" r="10" style="opacity:1;fill:#00b900;fill-opacity:1;fill-rule:nonzero;stroke:#ffff00;stroke-width:0.52206606;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1" > </circle>'; // inserto un nuevo elemento
-    var iglesia = a.getElementById('iglesia'); // selecciono el punto iglesia del dom del svg 
-        iglesia.addEventListener('click', function() {
-        tituloSubtitulo(iglesia)},false); // le asigno un evento 
+    puntos(cordenadas);
+
 });
