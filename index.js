3e35575b459c942bb74a0f522a92270c0c1fd24a
Carlos Alfonso <carlosalfonso144@gmail.com>
2017-01-10 03:37:07 +0000

    agregando presentacion inicio y creditos

---
+++ index.js
@@ -0,0 +1,36 @@
+﻿//  Menu con cambio de color
+function menu(color){
+    var a = document.getElementById("menu");
+    a.style.background = color;
+    a.className = ("");
+}
+function menuc(){
+    var a = document.getElementById("menu");
+    a.className = ("invisible");
+}
+// Para la presentación
+var contador=0;
+    function tiempo(){
+    var c = setInterval(inicio,2000);
+    }
+    function inicio(){
+        contador++;
+        var texto = document.getElementById("mover");
+        var desa = document.getElementById("principal");
+            switch(contador){
+                    case 1:
+                        texto.style.margin="0 0 0 -100%";
+                    break;
+                    case 2:
+                        texto.style.margin=" 0  0 0 -200%";
+                    break;
+                    
+                    case 3:
+                        desa.style.opacity="0.1";
+                    break;
+                    case 4:
+                        desa.style.display="none";
+                        window.location="app.html";
+                    break;
+                } 
+         }
